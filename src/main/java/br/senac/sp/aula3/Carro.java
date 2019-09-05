47e679d2ee0271ec96cf248add33798b213ba1db
Lukas Matias <lukaskt6@gmail.com>
2019-09-05 12:59:36 -0300

    tudo ok

--- src/main/java/br/senac/sp/aula3/Carro.java
+++ src/main/java/br/senac/sp/aula3/Carro.java
@@ -7,8 +7,54 @@ package br.senac.sp.aula3;
 
 /**
  *
- * @author fernando.fernandes
+ * @author lukas.mpereira
  */
 public class Carro {
-    
+
+    private String cor, fabricante, Modelo,gasolina;
+    private int velocidadeAtual;
+
+    String[] rodas = new String[4];
+
+    public Carro() {
+
+    }
+
+    Carro(int i) {
+        
+    }
+
+    public String getCor(String cor) {
+        return cor;
+    }
+
+    public void setCor(String pCor) {
+        this.cor = pCor;
+    }
+
+    String getModelo() {
+        return this.Modelo;
+
+    }
+
+    String getCor() {
+        return cor;
+    }
+
+
+   String descreve() {
+
+        if (this.Modelo == null) {
+            return "Um carro";
+        }
+        if (this.cor == null) {
+            return "Um " + this.Modelo;
+
+        }
+        return "Um " + this.Modelo + " na cor " + this.cor;
+    }
+
+    void setModelo(String seuOutroModelo) {
+        this.Modelo = seuOutroModelo;
+    }
 }
